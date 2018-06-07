9986357c5740b64b82ab3bbd35676091044fff29
vurtne <phoenixbudha@yeah.net>
2018-06-07 10:42:25 +0800

    添加一个OpenGL绘制的 banner 控件

--- wecore/src/main/java/com/freedom/wecore/widget/magical/MagicalRenderer.java
+++ wecore/src/main/java/com/freedom/wecore/widget/magical/MagicalRenderer.java
@@ -2,10 +2,15 @@ package com.freedom.wecore.widget.magical;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
 import android.opengl.Matrix;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -24,10 +29,14 @@ public class MagicalRenderer implements GLSurfaceView.Renderer {
     private int glHMatrix;
     private int hIsHalf;
     private int glHUxy;
+    private int hChangeType;
+    private int hChangeColor;
     private Bitmap mBitmap;
 
     private FloatBuffer bPos;
+    private FloatBuffer bMiniaturePos;
     private FloatBuffer bCoord;
+    private FloatBuffer bMiniatureCoord;
 
     private int textureId;
     private boolean isHalf;
@@ -40,12 +49,23 @@ public class MagicalRenderer implements GLSurfaceView.Renderer {
     private float[] mProjectMatrix=new float[16];
     private float[] mMVPMatrix=new float[16];
 
+    static final float[] sGary = new float[]{0.299f,0.587f,0.114f};
+    static final float[] sBlur = new float[]{0.006f,0.004f,0.002f};
+
     private final float[] sPos={
             -1.0f,1.0f,
             -1.0f,-1.0f,
             1.0f,1.0f,
             1.0f,-1.0f
     };
+
+    private final float[] sMiniaturePos={
+            -1.0f,-0.5f,
+            -1.0f,-0.96f,
+            1.0f,-0.5f,
+            1.0f,-0.96f
+    };
+
 
     private final float[] sCoord={
             0.0f,0.0f,
@@ -54,9 +74,54 @@ public class MagicalRenderer implements GLSurfaceView.Renderer {
             1.0f,1.0f,
     };
 
+    private final float[] sMiniatureCoord={
+            0.0f,0.75f,
+            0.0f,0.0f,
+            1.0f,0.75f,
+            1.0f,0.0f,
+    };
+//    private final float[] sMiniatureCoord={
+//            0.0f,0.75f,
+//            0.0f,0.08f,
+//            1.0f,0.75f,
+//            1.0f,0.98f,
+//    };
 
-    public MagicalRenderer(Context context) {
+
+    MagicalRenderer(Context context) {
         this.mContext = context;
+        this.mContext=context;
+        this.vertex="magical/half_color_vertex.sh";
+        this.fragment="magical/half_color_fragment.sh";
+        ByteBuffer bb=ByteBuffer.allocateDirect(sPos.length*4);
+        bb.order(ByteOrder.nativeOrder());
+        bPos=bb.asFloatBuffer();
+        bPos.put(sPos);
+        bPos.position(0);
+
+
+        ByteBuffer bb2=ByteBuffer.allocateDirect(sMiniaturePos.length*4);
+        bb2.order(ByteOrder.nativeOrder());
+        bMiniaturePos=bb2.asFloatBuffer();
+        bMiniaturePos.put(sMiniaturePos);
+        bMiniaturePos.position(0);
+
+        ByteBuffer cc=ByteBuffer.allocateDirect(sCoord.length*4);
+        cc.order(ByteOrder.nativeOrder());
+        bCoord=cc.asFloatBuffer();
+        bCoord.put(sCoord);
+        bCoord.position(0);
+
+        ByteBuffer c2=ByteBuffer.allocateDirect(sMiniatureCoord.length*4);
+        c2.order(ByteOrder.nativeOrder());
+        bMiniatureCoord=c2.asFloatBuffer();
+        bMiniatureCoord.put(sMiniatureCoord);
+        bMiniatureCoord.position(0);
+        try {
+            mBitmap = BitmapFactory.decodeStream(context.getResources().getAssets().open("magical/icon_head_bg.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -70,7 +135,7 @@ public class MagicalRenderer implements GLSurfaceView.Renderer {
         glHMatrix=GLES20.glGetUniformLocation(mProgram,"vMatrix");
         hIsHalf=GLES20.glGetUniformLocation(mProgram,"vIsHalf");
         glHUxy=GLES20.glGetUniformLocation(mProgram,"uXY");
-//        onDrawCreatedSet(mProgram);
+        onDrawCreatedSet(mProgram);
     }
 
     @Override
@@ -102,6 +167,72 @@ public class MagicalRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT|GLES20.GL_DEPTH_BUFFER_BIT);
+        GLES20.glUseProgram(mProgram);
+        onDrawSet2();
+        GLES20.glUniform1i(hIsHalf,isHalf?1:0);
+        GLES20.glUniform1f(glHUxy,uXY);
+        GLES20.glUniformMatrix4fv(glHMatrix,1,false,mMVPMatrix,0);
+        GLES20.glEnableVertexAttribArray(glHPosition);
+        GLES20.glEnableVertexAttribArray(glHCoordinate);
+        GLES20.glUniform1i(glHTexture, 0);
+        textureId=createTexture();
+        GLES20.glVertexAttribPointer(glHPosition,2,GLES20.GL_FLOAT,false,0,bPos);
+        GLES20.glVertexAttribPointer(glHCoordinate,2,GLES20.GL_FLOAT,false,0,bCoord);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP,0,4);
 
+        onDrawSet();
+        GLES20.glUniform1i(hIsHalf,isHalf?1:0);
+        GLES20.glUniform1f(glHUxy,uXY);
+        GLES20.glUniformMatrix4fv(glHMatrix,1,false,mMVPMatrix,0);
+        GLES20.glEnableVertexAttribArray(glHPosition);
+        GLES20.glEnableVertexAttribArray(glHCoordinate);
+        GLES20.glUniform1i(glHTexture, 0);
+        GLES20.glVertexAttribPointer(glHPosition,2,GLES20.GL_FLOAT,false,0,bMiniaturePos);
+        GLES20.glVertexAttribPointer(glHCoordinate,2,GLES20.GL_FLOAT,false,0,bMiniatureCoord);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP,0,4);
+    }
+
+    private void onDrawCreatedSet(int mProgram) {
+        hChangeType=GLES20.glGetUniformLocation(mProgram,"vChangeType");
+        hChangeColor=GLES20.glGetUniformLocation(mProgram,"vChangeColor");
+    }
+
+    private void onDrawSet() {
+        GLES20.glUniform1i(hChangeType,1);
+        GLES20.glUniform3fv(hChangeColor,1,sGary,0);
+    }
+
+    private void onDrawSet2() {
+        GLES20.glUniform1i(hChangeType,3);
+        GLES20.glUniform3fv(hChangeColor,1,sBlur,0);
+    }
+
+
+    public void setBitmap(Bitmap bitmap){
+        this.mBitmap = bitmap;
+    }
+
+
+    private int createTexture(){
+        int[] texture=new int[1];
+        if(mBitmap!=null&&!mBitmap.isRecycled()){
+            //生成纹理
+            GLES20.glGenTextures(1,texture,0);
+            //生成纹理
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,texture[0]);
+            //设置缩小过滤为使用纹理中坐标最接近的一个像素的颜色作为需要绘制的像素颜色
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER,GLES20.GL_NEAREST);
+            //设置放大过滤为使用纹理中坐标最接近的若干个颜色，通过加权平均算法得到需要绘制的像素颜色
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,GLES20.GL_TEXTURE_MAG_FILTER,GLES20.GL_LINEAR);
+            //设置环绕方向S，截取纹理坐标到[1/2n,1-1/2n]。将导致永远不会与border融合
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S,GLES20.GL_CLAMP_TO_EDGE);
+            //设置环绕方向T，截取纹理坐标到[1/2n,1-1/2n]。将导致永远不会与border融合
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T,GLES20.GL_CLAMP_TO_EDGE);
+            //根据以上指定的参数，生成一个2D纹理
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, mBitmap, 0);
+            return texture[0];
+        }
+        return 0;
     }
 }
